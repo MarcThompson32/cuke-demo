9c04886dfd907b4f0725eda8fc632b7383f4c893
marc <marcthompson32@gmail.com>
2016-04-21 21:28:51 -0500

    refactor constant values

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,7 +9,7 @@ $TIMEOUT = 20
 LOGIN_USERNAME_FAILURE = "Your username is invalid!"
 LOGIN_PASSWORD_FAILURE = "Your password is invalid!"
 LOGIN_SUCCESS = "You logged into a secure area!"
-CONFIRMATION_SENT = "Your 	e-mail's been sent!"
+CONFIRMATION_SENT = "Your e-mail's been sent!"
 
 HOVER1_TEXT = "name: user1\nView profile"
 HOVER2_TEXT = "name: user2\nView profile"
