a239c24cc2a2d53099dc5494b9dc97d2cc5383e0
marc <marcthompson32@gmail.com>
2016-04-22 22:13:42 -0500

    added ouput folder and simple comments to hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,9 +5,13 @@ require 'fig_newton'
 require 'data_magic'
 
 World(PageObject::PageFactory)
+
+#### CONSTANTS ####
 $TIMEOUT = 20
+
 LOGIN_USERNAME_FAILURE = "Your username is invalid!"
 LOGIN_PASSWORD_FAILURE = "Your password is invalid!"
+
 LOGIN_SUCCESS = "You logged into a secure area!"
 CONFIRMATION_SENT = "Your e-mail's been sent!"
 
